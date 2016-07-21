890668feb1d0aa2f5ebc0388501ff67c306285ba
Brian Anderson <banderson@mozilla.com>
2016-07-21 23:02:18 +0000

    Docs

---
+++ implementors/core/clone/trait.Clone.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["error_chain"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='error_chain/struct.Backtrace.html' title='error_chain::Backtrace'>Backtrace</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='backtrace/capture/struct.BacktraceFrame.html' title='backtrace::capture::BacktraceFrame'>BacktraceFrame</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='backtrace/capture/struct.BacktraceSymbol.html' title='backtrace::capture::BacktraceSymbol'>BacktraceSymbol</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/fmt/trait.Display.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["error_chain"] = ["impl&lt;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='struct' href='backtrace/symbolize/struct.SymbolName.html' title='backtrace::symbolize::SymbolName'>SymbolName</a>&lt;'a&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
